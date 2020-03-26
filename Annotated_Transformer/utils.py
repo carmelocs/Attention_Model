0ac8a36f807558de72d2b718e05f04ce959fc890
Shuai CHENG <melo89dec@hotmail.com>
2020-03-26 12:48:35 +1100

    utils

--- Annotated_Transformer/utils.py
+++ Annotated_Transformer/utils.py
@@ -27,3 +27,35 @@ def attention(query, key, value, mask=None, dropout=None):
     # the final scores: (batch, num_head, num_query, d_q)
     scores = torch.matmul(attn, value)
     return scores, attn
+
+def make_model(src_vocab, tgt_vocab, N=6, 
+               d_model=512, d_ff=2048, h=8, dropout=0.1):
+    "Helper: Construct a model from hyperparameters."
+    # src_vocab = 源语言词表大小
+    # tgt_vocab = 目标语言词表大小
+    
+    c = copy.deepcopy # 对象的深度copy/clone
+    attn = MultiHeadedAttention(h, d_model) # 8, 512
+    # 构造一个MultiHeadAttention对象
+    
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+    # 512, 2048, 0.1
+    # 构造一个feed forward对象
+
+    position = PositionalEncoding(d_model, dropout)
+    # 位置编码
+
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), 
+                             c(ff), dropout), N),
+        nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
+        Generator(d_model, tgt_vocab))
+
+    # This was important from their code. 
+    # Initialize parameters with Glorot / fan_avg.
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform(p)
+    return model # EncoderDecoder 对象
